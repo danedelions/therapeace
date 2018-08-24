a50cc7c384826aad1c3b5ab543113d9fc73c31f1
VChinChin <chino.boss31@gmail.com>
2018-08-25 01:10:27 +0800

    seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,51 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+    	'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'user_type' => 'therapist',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
 });
+
+// $factory->define(App\Client::class, function (Faker $faker) use ($factory) {
+//     return [
+//     	'user_id' => $factory->create(App\User::class)->id,
+//     	'fname' => $faker->firstName,
+// 	    'lname' => $faker->lastName,
+//     	'contact' => $faker->phoneNumber,
+//         'email' => $faker->unique()->safeEmail,
+//         'barangay' => $faker->city,
+//         'province' => $faker->city,
+//         'town' => $faker->city,
+//         'city' => $faker->city,
+//         'remember_token' => str_random(10),
+//     ];
+// });
+
+
+
+// $factory->define(App\Therapist::class, function (Faker $faker) use ($factory) {
+//     return [
+//     	'user_id' => $factory->create(App\User::class)->id,
+//     	'image' => $faker->imageUrl($width = 200, $height = 200),
+//     	'fname' => $faker->firstName,
+// 	    'lname' => $faker->lastName,
+//     	'contact' => $faker->phoneNumber,
+//         'barangay' => $faker->city,
+//         'province' => $faker->city,
+//         'town' => $faker->city,
+//         'city' => $faker->city,
+//         'therapist' => $faker->randomElement($array = array('occupational', 'physical')),
+//         'license' => $faker->randomDigit,
+//         'expiry_date' => $faker->dateTime,
+//         'license_image' => $faker->imageUrl($width = 200, $height = 200),
+//         'nbi_image' => $faker->imageUrl($width = 200, $height = 200),
+//         'bp_image' => $faker->imageUrl($width = 200, $height = 200),
+//         'remember_token' => str_random(10),
+//     ];
+// });
+
+ 
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // DB::table('users')->insert([
+        // 	'username' => 'peaceadmin',
+        //     'email' => 'therapeace@gmail.com',
+        //     'password' =>bcrypt('12345'),
+        //     'user_type' => 'admin'
+        // ]);
+
+         //factory(App\User::class,0)->create();
+         //factory(App\Client::class,5)->create();
+        // factory(App\Therapist::class,5)->create();
+
     }
 }
