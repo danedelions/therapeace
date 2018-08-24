1ec390568f937a4d58b001e607d96bf4892b8cde
tiffcen <39003588+tiffcen@users.noreply.github.com>
2018-08-25 01:59:05 +0800

    new

--- resources/views/client/account.blade.php
+++ resources/views/client/account.blade.php
@@ -1,4 +1,35 @@
-@extends('layouts.clienthead')
+@extends('layouts.cli')
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Therapeace | Client</title>
+        
+         <link rel="icon" href="img/logo.png" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link href="css/full-slider.css" rel="stylesheet">
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="jquery/jquery.min.js"></script>
+        <script src="https://unpkg.com/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    </head>
 
 @section('account')
  <h1>CLIENT ACCOUNT PAGE</h1>
--- resources/views/layouts/cli.blade.php
+++ resources/views/layouts/cli.blade.php
@@ -34,6 +34,29 @@
     </head>
 
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="/"><img src="img/logo.png" style="height: 50px;"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link nav-text" href="/client-find">Find</a>
+                    </li>
+                    <li class="nav-item">
+                     <a class="nav-link nav-text" href="/client-account">Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-text" href="/client-history">History</a>
+                    </li>
+                </ul>
+                <div class="buttons">
+                    <button class="btn btn-danger" style="margin: 5px;">Logout</button>
+                </div>
+            </div>
+        </nav>
+
         @yield('find')
         @yield('history')
         @yield('account')
