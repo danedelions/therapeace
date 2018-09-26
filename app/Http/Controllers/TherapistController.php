13613b86e09102d9717f21ac275a0bd797ae8219
danedelions <danielaechavez@gmail.com>
2018-09-26 18:11:57 +0800

    therapist update back-end

--- app/Http/Controllers/TherapistController.php
+++ app/Http/Controllers/TherapistController.php
@@ -73,9 +73,9 @@ class TherapistController extends Controller
         return ('login');
     }
 
-    public function edit($id)
+    public function edit(Therapist $therapist)
     {
-        return view('therapist.update');
+        return view('therapist.edit', compact('therapist'));
     }
 
     /**
@@ -85,20 +85,22 @@ class TherapistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TherapistRequest $request, Therapist $therapist)
     {
 
-        // Post::where('id',$id)->update($request->only(['title', 'blog_post']));
-        // return redirect()->route('admin.index');
+        $therapist->where('user_id', $therapist->id)->update($request->only(['image', 'fname', 'lname','contact', 'gender', 'barangay', 'province', 'town', 'city', 'therapist', 'license_number', 'license_image', 'expiry_date', 'nbi_image', 'bp_image']));
+        User::where('id', $therapist->id)->update($request->only(['username']));
+        return redirect()->('therapist.account');
 
+        // ClientProfile::where('client_id', $client->id)->update($request->only(['firstname','middlename','lastname','civil_status','birthday']));
         // $post->where('id', $post->id)->update($request->only(['title', 'blog_post']));
         // return redirect()->route('admin/');
 
     }
 
     public function therapistAccount(){
-
-        return view('therapist.account');
+        $therapist = Therapist::all();
+        return view('therapist.account', compact('therapist'));
     }
     public function therapistAppoint(){
 
@@ -111,10 +113,6 @@ class TherapistController extends Controller
     public function therapistMessage(){
 
         return view('therapist.message');
-    }
-       public function therapistEdit(){
-
-        return view('therapist.edit');
     }
  
 }
