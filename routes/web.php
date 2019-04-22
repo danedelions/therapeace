9b860c7dc2fd8ab1b66632afcfc6fbd0b8bf116e
judenino <canete.judenino@gmail.com>
2019-04-22 13:58:12 +0800

    rating stars

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,10 @@ Route::resource('client', 'ClientController');
 
 Route::group(['middleware' => 'auth'], function () {
     // ADMIN
-<<<<<<< HEAD
+
     Route::get('/admin-dashboard', 'AdminController@getDashboard')->name('get.dashboard');
     Route::get('/admin-login', 'AdminController@login')->name('get.login');
-=======
->>>>>>> 1561b589c174871de5e6e6ea44f5827fcc2a5d21
+
     Route::get('/admin-user', 'AdminController@getUserView')->name('get.view');
     Route::get('/admin-pending', 'AdminController@getPendingView')->name('get.pending');
     Route::get('/admin-history', 'AdminController@getHistoryView')->name('get.history');
